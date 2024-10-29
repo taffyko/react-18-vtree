afec0e51d23a1cdfd572004070cab7de21e95b82
taffyko <taffyko@protonmail.com>
2024-10-29 12:59:16 -0700

    chore: update package.json

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,10 @@
 export * from './FixedSizeTree';
 export * from './VariableSizeTree';
-export type { TreeWalker, TreeWalkerValue } from './Tree';
+export type {
+  TreeWalker,
+  TreeWalkerValue,
+  NodeComponentProps,
+  NodeRecord,
+  NodePublicState,
+} from './Tree';
 export { Row } from './Tree';
